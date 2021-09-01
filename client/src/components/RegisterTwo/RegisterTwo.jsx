cc3428d6d83c184aa4562905356208c1497d0716
FACU <facundoramirez089@gmail.com>
2021-09-01 13:40:26 -0300

    mejoras formRegister

--- client/src/components/RegisterTwo/RegisterTwo.jsx
+++ client/src/components/RegisterTwo/RegisterTwo.jsx
@@ -5,6 +5,7 @@ import register from '../../img/register.jpg';
 
 export default function RegisterTwo(){
     let inputRepeatEmail;
+    let inputRepeatPass;
 
     let [form, setForm] = useState({
         mail: '',
@@ -41,7 +42,9 @@ export default function RegisterTwo(){
         let value = e.target.value;
         let name = e.target.name;
         inputRepeatEmail = document.querySelector('.repeatMail');
+        inputRepeatPass = document.querySelector('.repeatPass');
 
+        // ================ PROCESO EMAIL, REPEAT EMAIL =====================
         if(name==='mail'){
             if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(value)){
                 setError({...error, [name]: 'Error'});
@@ -54,23 +57,70 @@ export default function RegisterTwo(){
             }
         }
         
-        setForm({...form, [name]: value, repeatMail: inputRepeatEmail.disabled ? '':inputRepeatEmail.value});
-        if(name==='repeatMail') validMail(e);
+        // =========================================================================
+        // ================ PROCESO PASSWORD, REPEAT PASSWORD ======================
+        if(name==='clave'){
+            let typedPass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+            if(!typedPass.test(value)){
+                setError({...error, [name]: 'Error'});
+                setPass({...pass, valid: false});
+                console.log(error, 'invalido');                
+                inputRepeatPass.disabled = true;
+            } else {
+                setError({...error, [name]: ''});
+                setPass({...pass, valid: true});
+                console.log(error, 'VALIDO');                
+                inputRepeatPass.disabled = false;
+            }
+        }
+
+        setForm({
+            ...form,
+            [name]: value,
+            repeatMail: inputRepeatEmail.disabled ? '':inputRepeatEmail.value,
+            repeatClave: inputRepeatPass.disabled ? '':inputRepeatPass.value
+        });
+
+        if(name==='repeatMail' || name==='repeatClave') validMail(e);
     }
 
     const validMail = (e) => {
         let value = e.target.value;
         let name = e.target.name;
         let mail = form.mail;
-
-        if(value === mail){
-            setEmail({...email, repeat:true});
-            setError({...error, [name]: ''});
-        }else{
-            setEmail({...email, repeat:false});
-            setError({...error, [name]: 'Error'});
+        let clave = form.clave;
+        
+        if(name==='repeatMail'){
+            if(value === mail){
+                setEmail({...email, repeat:true});
+                setError({...error, [name]: ''});
+            }else{
+                setEmail({...email, repeat:false});
+                setError({...error, [name]: 'Error'});
+            }
         }
+
+        if(name==='repeatClave'){
+            if(value === clave){
+                setPass({...pass, repeat:true});
+                setError({...error, [name]: ''});
+            }else{
+                setPass({...pass, repeat:false});
+                setError({...error, [name]: 'Error'});
+            }
+        }
+
     }
+
+    let [pass, setPass] = useState({
+        valid: false,
+        repeat: false
+    });
+
+    
+
+
+
 
     return(
         <div>
@@ -93,17 +143,13 @@ export default function RegisterTwo(){
                                 <h4 className={`${Style.repeatMailLabel} col-md-2 col-lg-2`}>Repetir Mail</h4>
                                 <input className={`${Style.repeatMail} repeatMail col-md-3 col-lg-4`} type="text" disabled name="repeatMail" value={form.repeatMail} onChange={(e)=> verifyMail(e)}/>
                             </div>
-                            {error.mail ?
+                            {error.mail && form.mail ?
                                 <div className={`row`}>
                                     <h5 className={`${Style.alertTexts} col-6`}>Introduza un correo válido</h5>
                                 </div>
                                 : null 
-                                // !email.repeat ?
-                                // <div className={`row`}>
-                                //     <h5 className={`${Style.alertTexts} col-6`}>Debe repetir el email correctamente</h5>
-                                // </div>:null
                             }
-                            {!error.mail && !email.repeat && document.querySelector('.repeatMail') ?
+                            {!error.mail && !email.repeat && form.repeatMail ?
                                 <div className={`row`}>
                                     <h5 className={`${Style.alertTexts2} col-9`}>Debe repetir exactamente el correo colocado</h5>
                                 </div>
@@ -111,11 +157,23 @@ export default function RegisterTwo(){
                             }
                             <div className={`row`}>
                                 <h4 className={`col-1`}>Clave</h4>
-                                <input className={`col-3`} type="text" />
+                                <input className={`col-3 pass`} type="text" name="clave" value={form.clave} onChange={(e)=> verifyMail(e)}/>
                                 {/* <h1 className={`col-1`}></h1> */}
                                 <h4 className={`${Style.repeatPass} col-3`}>Repetir Clave</h4>
-                                <input className={`${Style.repeatPassInp} repeatPass col-4`} type="text" />
+                                <input className={`${Style.repeatPassInp} repeatPass col-4`} type="text" name="repeatClave" value={form.repeatClave} onChange={(e)=> verifyMail(e)}disabled/>
                             </div>
+                            {error.clave && form.clave ?
+                                <div className={`row`}>
+                                    <h5 className={`${Style.alertTexts} col-6`}>Mínimo 8 caracteres, una letra y un número</h5>
+                                </div>
+                                : null 
+                            }
+                            {!error.clave && !pass.repeat && form.repeatClave ?
+                                <div className={`row justify-content-center`}>
+                                    <h5 className={`${Style.alertTexts2} col-5`}>Debe repetir exactamente la clave colocada</h5>
+                                </div>
+                                : null                                
+                            }
                             <div className={`row`}>
                                 <h4 className={`${Style.admLabel} col-sm-5 col-md-4 col-lg-4`}>Nombre del Administrador</h4>
                                 <input className={`${Style.inputLabel} col-sm-6 col-md-7 col-lg-7`} type="text" />
